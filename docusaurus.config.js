e3292dcba33cfc7a30a159b4d1d1807c55e0b61b
Tyler Sustare <10850753+TylerSustare@users.noreply.github.com>
2022-11-29 21:42:31 -0800

    Update copywrite and edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,7 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/TylerSustare/mysite/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -120,7 +119,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Tyler Sustare. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
